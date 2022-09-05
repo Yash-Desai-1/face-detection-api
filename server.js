6e17c42b56335f55866af249d5dbae9237281be0
Yash-Desai-1 <yashdesai4320@gmail.com>
2022-09-06 00:07:42 +0530

    second

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-
+const PORT= process.env.PORT || 3000;
 
 
 const db = knex({
@@ -32,7 +32,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('app is running on port 3000');
 })
 
